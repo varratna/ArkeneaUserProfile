674b5b5ccc71ee13d04f319b6f318578500b8a74
varratna <62686830+varratna@users.noreply.github.com>
2023-10-13 20:55:24 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArkeneaUserProfile.MvcApplication" Language="C#" %>
---
+++ Models/UserProfileEntities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace ArkeneaUserProfile.Models
+{
+    public class UserProfileEntities:DbContext
+    {
+        public UserProfileEntities() : base("UserProfile") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
